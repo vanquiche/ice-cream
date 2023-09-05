497f1393283795fa6648f6b89ee1cfc47c93f08a
vanquiche <stevevang2010@hotmail.com>
2023-09-05 21:37:56 +0900

    feat: enqueue scripts and stylesheets

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 1. enqueue styles and scripts
+ *
+ */
+
+
+/**
+ * enqueue styles and scripts
+ */
+function add_theme_styles_and_scripts()
+{
+  $theme_version = wp_get_theme()->get('Version');
+
+  wp_enqueue_style('theme-ice-cream-styles', get_template_directory_uri() . '/build/styles.css', array(), $theme_version);
+  wp_enqueue_script('theme-ice-cream-scripts', get_template_directory_uri() . '/build/script.js', array(), $theme_version, true);
+}
+
+add_action('wp_enqueue_scripts', 'add_theme_styles_and_scripts');
