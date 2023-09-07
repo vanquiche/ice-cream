a7ebc74a2ec1bd0f5f170b5b8ed7caf2668f6a10
vanquiche <stevevang2010@hotmail.com>
2023-09-07 14:32:59 +0900

    fix: escape placeholder text and category name

--- patterns/profile-banner.php
+++ patterns/profile-banner.php
@@ -3,8 +3,8 @@
 /**
  * Title: Profile Banner
  * Slug: ice-cream/profile-banner
- * Categories: banners
- * Keywords: Profile
+ * Categories: banner
+ * Keywords: Profile banner
  */
 ?>
 
@@ -16,14 +16,10 @@
   <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"backgroundColor":"base","textColor":"contrast","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","flexWrap":"wrap"}} -->
   <div class="wp-block-group has-contrast-color has-base-background-color has-text-color has-background" style="padding-top:0;padding-right:var(--wp--preset--spacing--40);padding-bottom:0;padding-left:var(--wp--preset--spacing--40)">
     <!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"typography":{"lineHeight":"1.3"}},"fontSize":"2-extra-large"} -->
-    <h2 class="wp-block-heading has-2-extra-large-font-size" style="margin-top:0;margin-bottom:0;line-height:1.3">
-      <?php __('Valar Morghulis', 'ice-cream') ?>
-    </h2>
+    <h2 class="wp-block-heading has-2-extra-large-font-size" style="margin-top:0;margin-bottom:0;line-height:1.3"><?php echo esc_html_x('Jaqen H\'ghar', 'ice-cream'); ?></h2>
     <!-- /wp:heading -->
     <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"typography":{"fontStyle":"italic","fontWeight":"300"}},"fontSize":"medium"} -->
-    <p class="has-medium-font-size" style="margin-top:0;margin-bottom:0;font-style:italic;font-weight:300">
-      <?php __('All men must die.', 'ice-cream') ?>
-    </p>
+    <p class="has-medium-font-size" style="margin-top:0;margin-bottom:0;font-style:italic;font-weight:300"><?php echo esc_html_x('All men must die.', 'ice-cream'); ?></p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
