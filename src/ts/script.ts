e10733dfb8775a870bb774d3900b53ff6e9c1ab8
vanquiche <stevevang2010@hotmail.com>
2023-11-08 14:03:19 +0900

    change id name

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,9 +1,8 @@
 import MobileMenu from './mobile-menu';
 
 if ( window !== undefined ) {
-	const mainNavigation = document.querySelector< HTMLElement >(
-		'#pd-main-navigation'
-	);
+	const mainNavigation =
+		document.querySelector< HTMLElement >( '#pd-main-menu' );
 	if ( mainNavigation ) {
 		const mobileMenu = new MobileMenu( mainNavigation );
 		mobileMenu.init();
