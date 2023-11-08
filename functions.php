f9408edba20cfa99dc5531591713b714cbf2e36b
vanquiche <stevevang2010@hotmail.com>
2023-11-08 13:54:04 +0900

    fix: incorrect script enqueue path

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ function add_theme_styles_and_scripts()
   $theme_version = wp_get_theme()->get('Version');
 
   wp_enqueue_style('theme-ice-cream-styles', get_template_directory_uri() . '/build/styles.css', array(), $theme_version);
-  wp_enqueue_script('theme-ice-cream-scripts', get_template_directory_uri() . '/build/script.js', array(), $theme_version, true);
+  wp_enqueue_script('theme-ice-cream-scripts', get_template_directory_uri() . '/build/scripts.js', array(), $theme_version, true);
 }
 
 add_action('wp_enqueue_scripts', 'add_theme_styles_and_scripts');
